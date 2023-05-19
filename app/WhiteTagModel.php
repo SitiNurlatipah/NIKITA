85025d43a3e405ea804de7501858d76a57c6b55e
Siti Nurlatipah <snurlatipah26@gmail.com>
2023-05-19 08:37:40 +0700

    push model untuk ceme dari lokal siti

--- app/WhiteTagModel.php
+++ app/WhiteTagModel.php
@@ -16,7 +16,7 @@ class WhiteTagModel extends Model
 
     public function score($id_user,$level)
     {
-        $counting = WhiteTagModel::select(DB::raw("COUNT(*) as cnt"),"level")
+        $counting = WhiteTagModel::select(DB::raw("sum(actual) as cnt"),DB::raw("sum(cd.target) as totaltarget"),"level","actual","target")
                                  ->join("users",function ($join) use ($id_user){
                                      $join->on("users.id","white_tag.id_user")
                                         ->where([
@@ -28,15 +28,36 @@ class WhiteTagModel extends Model
                                  ->join("curriculum as crclm","crclm.id_curriculum","cd.id_curriculum")
                                  ->where('crclm.level',$level)
                                  ->get();
-        if($counting[0]['level'] == 'B'){
-            $count = $counting[0]['cnt']/100*100;
-        }elseif($counting[0]['level'] == 'I')
-        {
-            $count = $counting[0]['cnt']/85*100;
+        $jumlahtarget = CompetenciesDirectoryModel::select(DB::raw("sum(competencies_directory.target) as totaltarget"),"level","nama_job_title","users.id_job_title")
+                                 ->join("job_title", "job_title.id_job_title", "=", "competencies_directory.id_job_title")
+                                 ->join("users","users.id_job_title","job_title.id_job_title")
+                                 ->join("curriculum as crclm", "crclm.id_curriculum", "=", "competencies_directory.id_curriculum")
+                                 ->where("crclm.level", "=", $level)
+                                 ->groupBy("competencies_directory.id_job_title")
+                                 // ->groupBy("users.id_job_title")
+                                //  ->where("competencies_directory.id_job_title", "=","users.id_job_title")
+                                 ->get();
+        // dd($jumlahtarget);
+        $cnt = $counting[0]["cnt"];
+        $target_total = $counting[0]["totaltarget"];
+        $actual = $counting[0]["cnt"];
+        // dd($counting);
+        if ($target_total != 0) {
+            if($counting[0]['level'] == 'B'){
+                $count = ($actual/$target_total)*100;
+            }elseif($counting[0]['level'] == 'I'){
+                $count = ($actual/$target_total)*85;
+            }else{
+                $count = ($actual/$target_total)*75;
+            } 
         }else{
-            $count = $counting[0]['cnt']/75*100;
+            $count = 0;
         }
-
+        if($count >= 100){
+            $count=100;
+        }else{
+            $count=$count;
+        }
         return $count;
     }
 
@@ -50,34 +71,47 @@ class WhiteTagModel extends Model
         $data = array();
         foreach($levels as $lv => $key)
         {
-           $wt = WhiteTagModel::select(DB::raw("COUNT(*) as cnt"),"level")
+           $wt = WhiteTagModel::select(DB::raw("sum(actual) as cnt"),DB::raw("sum(cd.target) as totaltarget"),"level","actual","target")
                                  ->join("users",function ($join) use ($id_user){
-                                     $join->on("users.id","white_tag.id_user")
+                                    $join->on("users.id","white_tag.id_user")
                                         ->where([
                                             ["white_tag.id_user",$id_user],
                                             ["white_tag.actual",">=","cd.target"]
                                         ]);
-                                 })
+                                    })
                                  ->join("competencies_directory as cd","cd.id_directory","white_tag.id_directory")
                                  ->join("curriculum as crclm","crclm.id_curriculum","cd.id_curriculum")
                                  ->where('crclm.level',$key)
                                  ->get();
-            $item = $wt[0]['cnt'];
+            $target = $wt[0]["totaltarget"];
+            $actual = $wt[0]["cnt"];
+            if ($target != 0) {
+                $item = ($actual/$target)*100;
+            }else{
+                $item = 0;
+            }
             array_push($data,$item);
         }
+        
         $data2 = array_sum($data)/3;
-        // if($data2 >= 81.37)
-        // {
-        //     // set is competent = 1
-        //     $user = User::find($id_user);
-        //     $user->is_competent = 1;
-        //     $user->save();
-        // }else{
-        //     // set is competent = 0
-        //     $user = User::find($id_user);
-        //     $user->is_competent = 0;
-        //     $user->save();
-        // }
+        if($data2 >= 100){
+            $data2=100;
+        }else{
+            $data2=$data2;
+        }
+        if($data2 >= 81.37)
+        {
+            // set is competent = 1
+            $user = User::find($id_user);
+            $user->is_competent = 1;
+            $user->save();
+        }else{
+            // set is competent = 0
+            $user = User::find($id_user);
+            $user->is_competent = 0;
+            $user->save();
+        }
+        
         return $data2;
     }
 
