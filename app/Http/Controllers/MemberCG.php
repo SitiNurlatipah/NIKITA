8ba0d351cdcfd65160da66a40e436385c070a28a
diyorossi <diyorossi@gmail.com>
2023-05-23 08:10:14 +0700

    update validation add user

--- app/Http/Controllers/MemberCG.php
+++ app/Http/Controllers/MemberCG.php
@@ -60,7 +60,7 @@ class MemberCG extends Controller
         $request->validate([
             'image' => 'nullable|image|mimes:jpg,png,jpeg|max:5000',
             'base64' => 'nullable|string',
-            'nik' => 'required|numeric',
+            'nik' => 'required',
             'password' => 'required',
             'peran_pengguna' => 'required|in:1,2,3',
             'tgl_masuk' => 'required',
