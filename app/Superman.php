71f1bf760c25f07f57d270889cfb7647cdf591b3
Siti Nurlatipah <snurlatipah26@gmail.com>
2023-11-30 13:37:04 +0700

    Memperbaiki perhitungan ceme superman

--- app/Superman.php
+++ app/Superman.php
@@ -18,10 +18,7 @@ class Superman extends Model
         $user = User::select("id")
                 ->where("id", $id_user)
                 ->first();
-        $skilId = [1,2];
-        $data = array();
-        foreach($skilId as $lv => $key)
-        {
+        
             $wt = CompDictionarySupermanModel::select(
                     DB::raw("SUM(IFNULL(competencies_superman.actual, 0)) as total_actual"),
                     DB::raw("SUM(competencies_dictionary_superman.target) as total_target")
@@ -34,7 +31,7 @@ class Superman extends Model
                     $join->on("competencies_superman.id_cstu", "=", "competencies_dictionary_superman.id_dictionary_superman")
                         ->where("competencies_superman.id_user", $id_user);
                 })
-                ->where('curriculum_superman.id_skill_category',$key)
+                // ->where('curriculum_superman.id_skill_category',$key)
                 ->get();
            $jmlactual = Superman::select(DB::raw("sum(actual) as cnt"),DB::raw("sum(cd.target) as totaltarget"),"actual","target")
                                  ->join("users",function ($join) use ($id_user){
@@ -46,7 +43,7 @@ class Superman extends Model
                                     })
                                  ->join("competencies_dictionary_superman as cd","cd.id_dictionary_superman","competencies_superman.id_cstu")
                                  ->join("curriculum_superman as crclm","crclm.id_curriculum_superman","cd.id_curriculum_superman")
-                                 ->where('crclm.id_skill_category',$key)
+                                //  ->where('crclm.id_skill_category',$key)
                                  ->get();
             $target = $wt[0]["total_target"];
             $actual = $jmlactual[0]["cnt"];
@@ -55,16 +52,16 @@ class Superman extends Model
             }else{
                 $item = 0;
             }
-            array_push($data,$item);
-        }
+            // array_push($data,$item);
         
-        $data2 = array_sum($data)/2;
-        if($data2 >= 100){
-            $data2=100;
+        
+        // $data2 = array_sum($data)/2;
+        if($item >= 100){
+            $item=100;
         }else{
-            $data2=$data2;
+            $item=$item;
         }
-        if($data2 >= 86.67)
+        if($item >= 86.67)
         {
             // set is competent = 1
             $user = User::find($id_user);
@@ -77,6 +74,6 @@ class Superman extends Model
             $user->save();
         }
         
-        return $data2;
+        return $item;
     }
 }
